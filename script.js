fddffedde781961c28d6d0f91a7b71e21f0a6a17
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2025-09-02 19:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let inputs=document.querySelectorAll(".code");
+
+inputs.forEach((input,index)=>{
+	input.addEventListener("input",()=>{
+		if(input.value && index<inputs.length-1){
+			inputs[index+1].focus();
+		}
+	});
+	input.addEventListener("keydown",(e)=>{
+		if(e.key==="Backspace"&& !input.value && index>0){
+			inputs[index-1].focus();
+			inputs[index-1].value="";
+		}
+	})
+})
+
+
